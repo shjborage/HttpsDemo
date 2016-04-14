c58e640e4cc9ecbcee7a92cd17f4465f7e8bb4fd
Eric <shjborage@gmail.com>
2016-04-14 15:52:35 +0800

    【ADD】 init project

---
+++ HttpsDemo/HttpsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HttpsDemo
+//
+//  Created by shihaijie on 4/14/16.
+//  Copyright © 2016 Saick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HttpsDemo/HttpsDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HttpsDemo
+//
+//  Created by shihaijie on 4/14/16.
+//  Copyright © 2016 Saick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
