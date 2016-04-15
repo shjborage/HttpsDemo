72a7e6462a9226bc54e928e71e74a5fa3cd65b93
Eric <shjborage@gmail.com>
2016-04-15 15:19:57 +0800

    尝试加入证书验证以及相关说明

--- HttpsDemo/HttpsDemo/ViewController.m
+++ HttpsDemo/HttpsDemo/ViewController.m
@@ -12,6 +12,8 @@
 
 @property (nonatomic, strong) NSURLConnection *connection;
 
+@property (nonatomic, strong) NSArray *trustedCertificates;
+
 @end
 
 @implementation ViewController
@@ -25,14 +27,22 @@
     [super viewWillAppear:animated];
     
     // Now start the connection
-    NSURL *httpsURL = [NSURL URLWithString:@"https://www.github.com"];
+//    NSURL *httpsURL = [NSURL URLWithString:@"https://www.github.com"];
+    NSURL *httpsURL = [NSURL URLWithString:@"https://sp0.baidu.com/80MWsjS6_wk28tyhnq/"];
+//    NSURL *httpsURL = [NSURL URLWithString:@"https://www.baidu.com/"];
     self.connection = [NSURLConnection connectionWithRequest:[NSURLRequest requestWithURL:httpsURL] delegate:self];
     
-//    //先导入证书
-//    NSString * cerPath = ...; //证书的路径
-//    NSData * cerData = [NSData dataWithContentsOfFile:cerPath];
-//    SecCertificateRef certificate = SecCertificateCreateWithData(NULL, (__bridge CFDataRef)(cerData));
-//    self.trustedCertificates = @[CFBridgingRelease(certificate)];
+    //先导入证书
+    NSString *cerPath = [[NSBundle mainBundle] pathForResource:@"cert" ofType:@"cer"]; //证书的路径
+    NSData *cerData = [NSData dataWithContentsOfFile:cerPath];
+    SecCertificateRef certificate = SecCertificateCreateWithData(NULL, (__bridge CFDataRef)(cerData));
+    NSString *cerPath1 = [[NSBundle mainBundle] pathForResource:@"cert_p" ofType:@"cer"]; //证书的路径
+    NSData *cerData1 = [NSData dataWithContentsOfFile:cerPath1];
+    SecCertificateRef certificate1 = SecCertificateCreateWithData(NULL, (__bridge CFDataRef)(cerData1));
+    NSString *cerPath2 = [[NSBundle mainBundle] pathForResource:@"cert_pp" ofType:@"cer"]; //证书的路径
+    NSData *cerData2 = [NSData dataWithContentsOfFile:cerPath2];
+    SecCertificateRef certificate2 = SecCertificateCreateWithData(NULL, (__bridge CFDataRef)(cerData2));
+    self.trustedCertificates = @[CFBridgingRelease(certificate), CFBridgingRelease(certificate1), CFBridgingRelease(certificate2)];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -42,37 +52,12 @@
 
 #pragma mark - NSURLConnectionDataDelegate
 
-- (void)connection:(NSURLConnection *)connection willSendRequestForAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge {
-    //1)获取trust object
-    SecTrustRef trust = challenge.protectionSpace.serverTrust;
-    SecTrustResultType result;
-    
-    //2)SecTrustEvaluate对trust进行验证
-    OSStatus status = SecTrustEvaluate(trust, &result);
-    if (status == errSecSuccess &&
-        (result == kSecTrustResultProceed || result == kSecTrustResultUnspecified))
-    {
-        //3)验证成功，生成NSURLCredential凭证cred，告知challenge的sender使用这个凭证来继续连接
-        NSURLCredential *cred = [NSURLCredential credentialForTrust:trust];
-        [challenge.sender useCredential:cred forAuthenticationChallenge:challenge];
-        
-        NSLog(@"Https 单向验证成功");
-    } else {
-        //5)验证失败，取消这次验证流程
-        [challenge.sender cancelAuthenticationChallenge:challenge];
-    }
-}
-
-//// 使用本地证书的方式
 //- (void)connection:(NSURLConnection *)connection willSendRequestForAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge {
 //    //1)获取trust object
 //    SecTrustRef trust = challenge.protectionSpace.serverTrust;
 //    SecTrustResultType result;
 //    
-//    //注意：这里将之前导入的证书设置成下面验证的Trust Object的anchor certificate
-//    SecTrustSetAnchorCertificates(trust, (__bridge CFArrayRef)self.trustedCertificates);
-//    
-//    //2)SecTrustEvaluate会查找前面SecTrustSetAnchorCertificates设置的证书或者系统默认提供的证书，对trust进行验证
+//    //2)SecTrustEvaluate对trust进行验证
 //    OSStatus status = SecTrustEvaluate(trust, &result);
 //    if (status == errSecSuccess &&
 //        (result == kSecTrustResultProceed || result == kSecTrustResultUnspecified))
@@ -80,11 +65,44 @@
 //        //3)验证成功，生成NSURLCredential凭证cred，告知challenge的sender使用这个凭证来继续连接
 //        NSURLCredential *cred = [NSURLCredential credentialForTrust:trust];
 //        [challenge.sender useCredential:cred forAuthenticationChallenge:challenge];
+//        
+//        NSLog(@"Https 单向验证成功");
 //    } else {
 //        //5)验证失败，取消这次验证流程
 //        [challenge.sender cancelAuthenticationChallenge:challenge];
 //    }
 //}
+
+// 使用本地证书的方式
+- (void)connection:(NSURLConnection *)connection willSendRequestForAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge {
+    //1)获取trust object
+    SecTrustRef trust = challenge.protectionSpace.serverTrust;
+    SecTrustResultType result;
+    
+    // 判断是否开启双向验证
+    NSString *method = challenge.protectionSpace.authenticationMethod;
+//    NSURLAuthenticationMethodServerTrust
+//    NSURLAuthenticationMethodClientCertificate
+    
+    //注意：这里将之前导入的证书设置成下面验证的Trust Object的anchor certificate
+    SecTrustSetAnchorCertificates(trust, (__bridge CFArrayRef)self.trustedCertificates);
+    
+    //2)SecTrustEvaluate会查找前面SecTrustSetAnchorCertificates设置的证书或者系统默认提供的证书，对trust进行验证
+    OSStatus status = SecTrustEvaluate(trust, &result);
+    if (status == errSecSuccess &&
+        (result == kSecTrustResultProceed || result == kSecTrustResultUnspecified))
+    {
+        //3)验证成功，生成NSURLCredential凭证cred，告知challenge的sender使用这个凭证来继续连接
+        NSURLCredential *cred = [NSURLCredential credentialForTrust:trust];
+
+//        NSURLCredential *credCer = [NSURLCredential credentialWithIdentity:nil certificates:self.trustedCertificates persistence:NSURLCredentialPersistenceNone];
+        [challenge.sender useCredential:cred forAuthenticationChallenge:challenge];
+    } else {
+        //5)验证失败，取消这次验证流程
+        [challenge.sender cancelAuthenticationChallenge:challenge];
+//        [self showPreferencesCredentialsAreIncorrectPanel:self];
+    }
+}
 
 - (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
 {
--- README.md
+++ README.md
@@ -2,6 +2,36 @@
 
 在 iOS 中尝试 https  
 
+### 根据域名下载证书
+证书其实就是文本，可通过以下命令来获取并手动保存。 多级证书，可能需要保存多个并分别操作。
+```
+openssl s_client -showcerts -host sp0.baidu.com -port 443
+```
+>You can examine one of these certificates by:
+>
+>1. copying the text (the -----BEGIN CERTIFICATE----- line through to the -----END CERTIFICATE----- line) into a text file with the .pem extension
+>2. dragging that file into Keychain Access
+>3. double clicking the newly imported certificate
+>This will open the standard certificate viewer which you can use to examine the certificate in detail.
+
+这儿也有一些说明：<http://serverfault.com/questions/139728/how-to-download-ssl-certificate-from-a-website>
+
+
+证书转换格式供 iOS 的工程使用
+
+```
+openssl x509 -in sp0_bdnsdk.gem -out cert.cer -outform der
+```
+
+### 双向验证
+双向验证一般比较少，当服务端需要双向验证的话， `connection: willSendRequestForAuthenticationChallenge:` 返回的`challenge.protectionSpace.authenticationMethod` 是 `NSURLAuthenticationMethodClientCertificate`，处理的方式是使用 `credentialWithIdentity:certificates:persistence:` 来获取本地证书创建凭证 `credential`，使用该凭证来进行连接。
+
+具体你可以参考下面文档和例子：  
+<https://developer.apple.com/library/ios/documentation/Cocoa/Conceptual/URLLoadingSystem/Articles/AuthenticationChallenges.html>  
+<http://www.sunethmendis.com/2013/01/11/certificate-based-client-authentication-in-ios/>
+<http://stackoverflow.com/questions/25924881/ios-client-certificates-and-mobile-device-management>
+
 ##REF
+<https://developer.apple.com/library/ios/technotes/tn2232/_index.html>
 <http://nsscreencast.com/episodes/73-ssl-pinning>
 <http://oncenote.com/2014/10/21/Security-1-HTTPS/>
